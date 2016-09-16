50bd6c1bb605543c9c5c346b5252b7d2ab07ced3
afoolboy <zy516668272@gmail.com>
2016-09-16 21:54:27 +0800

    a ez ftp server for file transport

---
+++ ftpserver.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+import socket
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-u","--user",dest="username",help="login username")
+parser.add_option("-p","--password",dest="password",help="login password")
+parser.add_option("-d","--dir",dest="homedir",help="ftp homedir")
+parser.add_option("-l","--port",dest="port",help="ftp listen port")
+
+(options,args) = parser.parse_args()
+
+default_username = "admin"
+default_password = "123456"
+default_homedir = "."
+default_port = 21 
+
+print options
+
+if options.username:
+	default_username=options.username
+
+if options.password:
+	default_password=options.password
+
+if options.homedir:
+	default_homedir = options.homedir
+
+if options.port:
+	default_port=options.port
+
+
+
+authorizer = DummyAuthorizer()
+authorizer.add_user(default_username, default_password, default_homedir)
+handler = FTPHandler
+handler.authorizer = authorizer
+server = FTPServer(("0.0.0.0",default_port), handler)
+server.serve_forever()
+
+
+
